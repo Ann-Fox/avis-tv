f4078deec1dcc0b8cbc02b3332bec4ac87dfe0ab
Anna Chernysheva <dx8565@gmail.com>
2023-12-21 15:47:40 +0500

    chore: working with polymer arrays and objects (v-for)

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
+<!--2 работа с реактивными массивами и объектами (v-for)-->
+<script setup>
+import {ref} from "vue";
+
+const users = ref ([
+  {id: 1, name: 'Эмиль', age: 25},
+  {id: 2, name: 'Eva', age: 125},
+  {id: 3, name: 'Vally', age: 225},
+])
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users"
+      :key="user.id"
+      >
+      {{ user.name}}
+        <sup>{{ user.age}}</sup>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+
+<!--1 основы с реактивностью (ref, reactive, события)-->
+<!--
 <script setup>
 import { reactive } from "vue";
 
@@ -18,3 +46,4 @@ const increment = () => {
   </div>
 </template>
 
+-->
