022bb41cf32373fe4d3d7e9256416c77b0d00981
Anna Chernysheva <dx8565@gmail.com>
2023-12-21 15:35:01 +0500

    git init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { reactive } from "vue";
+
+const counter = reactive({
+  count: 5
+});
+const increment = () => {
+  counter.count++
+}
+</script>
+
+<template>
+  <div>
+    <button @click="increment">
+      Count:
+      <strong>{{ counter.count }}</strong>
+    </button>
+  </div>
+</template>
+
